e520df4e9e0917d70b52cacc6cf43649ceb6e072
Ramiro Raffo <rraffo@fi.uba.ar>
2026-04-17 02:06:34 -0300

    Adds a file lost in the previous commit

--- shell/.shell/aliases.sh
+++ shell/.shell/aliases.sh
@@ -8,9 +8,15 @@ alias iv='nvim'
 alias sl='ls'
 alias la='ls -a'
 alias ll='ls -al'
+#if eza
+if command -v eza &> /dev/null; then
+    alias ls='eza'
+    alias eza='eza --icons'
+fi
 
 #clear
 alias cl='clear'
 
 #tree
 alias tree='tree -I ".git"'
+
